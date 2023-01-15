2592f5a62ba930b717ccd55f95a1ee7e8c5871e1
PawełNiewęgłowski <pawelnwg@gmail.com>
2023-01-15 15:27:36 +0100

    Fix lock

--- OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Asynchronous.cs
+++ OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Asynchronous.cs
@@ -5,52 +5,44 @@ using System.Diagnostics;
 using System.Text;
 using static System.Linq.Enumerable;
 
-Random random = new Random();
-
 Polynominal[] polynominals = new Polynominal[Configuration.polynominalsCount];
 int k = 1;
 var stopwatch = new Stopwatch();
 stopwatch.Start();
 
 for (int algorithmIteration = 0; algorithmIteration < Configuration.algorithmIterations; algorithmIteration++, k++)
-{
-     polynominals = ParallelPart_1();
-}
+    polynominals = IslandModelParallel();
 
 stopwatch.Stop();
 Console.WriteLine("Time taken: " + stopwatch.Elapsed.ToString(@"m\:ss\.fff" + "s"));
 
-// mozna zasosowac model wyspowy i zlaczyc tylko na koncu 
+// model wyspowy 
 
-Polynominal[] ParallelPart_1()
+Polynominal[] IslandModelParallel()
 {
-    List<List<Polynominal>> polynominal = new List<List<Polynominal>>();
+    ConcurrentBag<List<Polynominal>> polynominal = new ConcurrentBag<List<Polynominal>>();
     List<Thread> threads = new List<Thread>();
 
     if (k == 1)
         polynominals = Preparation.PrepareFirstGeneration(Configuration.polynominalsCount);
 
     var chunkedPolinominals = polynominals.Chunk(Configuration.elementsPerChunk).ToArray();
-    Console.WriteLine(polynominals.Count() + " " + k);
 
     foreach (var index in Range(0, Configuration.threadCount))
-    {
-        threads.Add(new Thread(() => { polynominal.Add(ParallelThreadWork_1(chunkedPolinominals[index])); }));
-    }
+        threads.Add(new Thread(() => { polynominal.Add(ParallelThreadWork(chunkedPolinominals[index])); }));
 
     threads.ForEach(x => x.Start());
     threads.ForEach(x => x.Join());
-    while (polynominal.Count != Configuration.threadCount)
-        Thread.Sleep(1);
     return polynominal.SelectMany(x => x).ToArray();
 }
 
-List<Polynominal> ParallelThreadWork_1(Polynominal[] polynominalsChunked)
+List<Polynominal> ParallelThreadWork(Polynominal[] polynominalsChunked)
 {
     polynominalsChunked = Mutation.MautatePolynominals(polynominalsChunked);
     polynominalsChunked = Crossover.CrossoverPolynominals(polynominalsChunked);
     polynominalsChunked = Fitness.CalculateFitness(polynominalsChunked);
     polynominalsChunked = Selection.SelectBestPolynominals(polynominalsChunked);
+    Console.WriteLine($"Pokolenie {k}, najlepszy osobnik {polynominalsChunked.First().FintessValue}, wyspa {Thread.CurrentThread.ManagedThreadId}"); // only for Debug
     polynominalsChunked = Population.FillPolynominals(polynominalsChunked, polynominalsChunked.First().Elements.Select(x => x.Coefficient).ToList());
 
     return polynominalsChunked.ToList();
@@ -72,7 +64,3 @@ public static class EnumExtension
     public static IEnumerable<(T item, int index)> WithIndex<T>(this IEnumerable<T> self)
        => self.Select((item, index) => (item, index));
 }
-
-//var res = await Task.Factory.StartNew(Work_part_1);
-//var p = polynominals.ToList().ChunkBy(threadCount);
-// split
