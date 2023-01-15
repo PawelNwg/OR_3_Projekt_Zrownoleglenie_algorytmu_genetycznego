159340dd8c7be8317f9032a3ce78e850dd6e515e
Mateusz Uszyński <m.uszynski@futuremind.com>
2023-01-15 19:32:00 +0100

    Revert "Parallel foreach"
    This reverts commit c66469f9cffefadd73d7705757637c5df2d446fd.

--- OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Asynchronous.cs
+++ OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Asynchronous.cs
@@ -6,139 +6,17 @@ using System.Text;
 using static System.Linq.Enumerable;
 
 Polynominal[] polynominals = new Polynominal[Configuration.polynominalsCount];
-ParallelOptions options = new ParallelOptions {MaxDegreeOfParallelism = Configuration.threadCount};
-int k = 0;
-Random random = new Random();
+int k = 1;
 var stopwatch = new Stopwatch();
 stopwatch.Start();
 
-foreach (var i in Range(0, Configuration.algorithmIterations - 1))
-{
-    k++;
-    polynominals = IslandModelParallelForeach();
-}
+for (int algorithmIteration = 0; algorithmIteration < Configuration.algorithmIterations; algorithmIteration++, k++)
+    polynominals = IslandModelParallel();
 
 stopwatch.Stop();
 Console.WriteLine("Time taken: " + stopwatch.Elapsed.ToString(@"m\:ss\.fff" + "s"));
 
 // model wyspowy 
-// PARALLEL FOREACH START
-Polynominal[] IslandModelParallelForeach()
-{
-    ConcurrentBag<List<Polynominal>> polynominalBag = new ConcurrentBag<List<Polynominal>>();
-    
-    if (k == 1)
-        polynominals = Preparation.PrepareFirstGeneration(Configuration.polynominalsCount);
-    
-    var chunkedPolinominals = polynominals.Chunk(Configuration.elementsPerChunk).ToArray();
-
-    Parallel.ForEach(chunkedPolinominals, options,(line, _, _) =>
-    {
-        polynominalBag.Add(ParallelWork(line));
-    });
-    
-    return polynominalBag.SelectMany(x => x).ToArray();
-}
-
-List<Polynominal> ParallelWork(Polynominal[] polynominalsChunked)
-{
-    polynominalsChunked = MautatePolynominals(polynominalsChunked);
-    polynominalsChunked = Crossover.CrossoverPolynominals(polynominalsChunked);
-    polynominalsChunked = CalculateFitnessV2(polynominalsChunked);
-    polynominalsChunked = Selection.SelectBestPolynominals(polynominalsChunked);
-    Console.WriteLine($"Pokolenie {k}, najlepszy osobnik {polynominalsChunked.First().FintessValue}, wyspa {Thread.CurrentThread.ManagedThreadId}"); // only for Debug
-    polynominalsChunked = FillPolynominals(polynominalsChunked, polynominalsChunked.First().Elements.Select(x => x.Coefficient).ToList());
-
-    return polynominalsChunked.ToList();
-}
-
-Polynominal[] MautatePolynominals(Polynominal[] polynominals)
-{
-    int numberOfPolynominalsToMutate = (int)(polynominals.Length * Configuration.percentPopulationToMutate);
-    if (numberOfPolynominalsToMutate == 0)
-        return polynominals;
-
-    for (int i = 0; i < numberOfPolynominalsToMutate; i++)
-    {
-        var index3 = random.Next(0, polynominals.Length);
-        polynominals[index3] = MutatePolynominal(polynominals[i]);
-    }
-    return polynominals;
-}
-
-Polynominal MutatePolynominal(Polynominal polynominal)
-{
-    Parallel.ForEach(polynominal.Elements, options, (element, _, _) =>
-    {
-        element.Exponent = random.NextDouble() * (1.0 - (-1.0)) + (-1.0);
-    });
-    
-    return polynominal;
-}
-
-Polynominal[] CalculateFitness(Polynominal[] polynominals)
-{
-    Parallel.ForEach(polynominals, options,(element, _, _) =>
-    {
-        var tempSum = 0.0;
-        foreach (var elementElement in element.Elements)
-        {
-            tempSum += elementElement.Coefficient * elementElement.Exponent;
-        }
-        element.FintessValue = tempSum;
-    });
-    
-    return polynominals;
-}
-
-Polynominal[] CalculateFitnessV2(Polynominal[] polynominals)
-{
-    Parallel.ForEach(polynominals, options,(polinominal, _, _) =>
-    {
-        ConcurrentBag<double> bag = new ConcurrentBag<double>();
-
-        Parallel.ForEach(polinominal.Elements, options,(element, _, _) =>
-        {
-            bag.Add(element.Coefficient * element.Exponent);
-        });
-
-        polinominal.FintessValue = bag.Sum();
-    });
-    
-    return polynominals;
-}
-
-Polynominal[] FillPolynominals(Polynominal[] polynominals, List<Double> coefficients)
-{
-    if (polynominals.Length == Configuration.polynominalsCount)
-        return polynominals;
-
-    var polynominalsToFill = Configuration.elementsPerChunk - polynominals.Length;
-    Polynominal[] newPolynominals = new Polynominal[polynominalsToFill];
-
-
-
-    for (int i = 0; i < polynominalsToFill; i++)
-    {
-        var newPolynominal = new Polynominal();
-        var range = Range(0, Configuration.polynominalsCount - 1);
-
-        ConcurrentBag<PolynominalElement> polynominalElements = new ConcurrentBag<PolynominalElement>();
-
-        Parallel.ForEach(range, options, (polynominal, _, index) =>
-        {
-            var polynominalElement = new PolynominalElement()
-                {Coefficient = coefficients[(int) index], Exponent = 50.0 * (1.0 - (-1.0)) + (-1.0)};
-
-            polynominalElements.Add(polynominalElement);
-        });
-        newPolynominal.Elements.AddRange(polynominalElements.ToArray());
-        newPolynominals[i] = newPolynominal;
-    }
-
-    return polynominals.Concat(newPolynominals).ToArray();
-}
-// PARALLEL FOREACH STOP
 
 Polynominal[] IslandModelParallel()
 {
--- OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Configuration/Configuration.cs
+++ OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async/Configuration/Configuration.cs
@@ -8,12 +8,12 @@ namespace OR_3_Projekt_Zrownoleglenie_alg_genetycznego_wersja_async
 {
     public static class Configuration
     {
-        public static readonly int polynominalsCount = 2048; // S
-        public static readonly int algorithmIterations = 1024;// (int)(1024 * 1024 / polynominalsCount) - (polynominalsCount + 10); // N
+        public static readonly int polynominalsCount = 1024; // S
+        public static readonly int algorithmIterations = 512;// (int)(1024 * 1024 / polynominalsCount) - (polynominalsCount + 10); // N
         public static readonly double percentPopulationToMutate = 0.1;
         public static readonly double percentPopulationToCrossover = 0.1;
         public static readonly double percentPopulationToPromoteToNextGeneration = 0.8;
-        public static readonly int threadCount = 2;
+        public static readonly int threadCount = 4;
         public static readonly int elementsPerChunk = Configuration.polynominalsCount / threadCount;
     }
 }
